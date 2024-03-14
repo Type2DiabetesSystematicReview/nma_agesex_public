fcda2874c96993b3805ccfc9091466e3c8bcd90e
David McAllister <david.mcallister@glasgow.ac.uk>
2024-03-14 20:17:52 +0000

    Minor changes

--- 09e_run_mace_class.R
+++ 09e_run_mace_class.R
@@ -17,6 +17,8 @@ cfs <- cfs %>%
 
 mace_agg <- mace_agg %>% 
   mutate(arm_lvl = trtcls5)
+mace_agg_sex <- mace_agg_sex %>% 
+  mutate(arm_lvl = trtcls5)
 pseudo <- pseudo %>% 
   mutate(arm_lvl = trtcls5)
 cfs <- cfs %>% 
@@ -36,10 +38,16 @@ pseudo <- pseudo %>%  add_integration(
 mycor <- pseudo$int_cor
 rm(pseudo)
 
-nwork <- combine_network(set_agd_contrast(data = mace_agg,
-                                            study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "place", 
-                                            trt_class = trtcls5, sample_size = participants), 
+nwork <- combine_network(set_agd_contrast(data = mace_agg_sex,
+                                          study = paste0(nct_id, "_", level_cat), 
+                                          trt = arm_lvl, y = loghr, se = se, 
+                                          trt_ref = "place", 
+                                          trt_class = trtcls5, sample_size = participants), 
+                         set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_sex$nct_id),
+                                          study = nct_id, 
+                                          trt = arm_lvl, y = loghr, se = se, 
+                                          trt_ref = "place", 
+                                          trt_class = trtcls5, sample_size = participants), 
                            set_agd_regression(cfs,
                                               study = nct_id,
                                               trt = arm_lvl,
@@ -54,8 +62,11 @@ nwork <- combine_network(set_agd_contrast(data = mace_agg,
 ## Add integration points. Note taking correlations from pseudo IPD networks
 # Note - approximately centre but do not scale age by subtracting 60
 nwork <-  add_integration(nwork,
-                          age = distr(qtruncnorm,  a = min_age-60, 
-                                      b = max_age-60, mean = age_mu-60, sd = age_sigma),
+                          age = distr(qtruncnorm,  
+                                      a = min_age-60, 
+                                      b = max_age-60,
+                                      mean = age_mu-60, 
+                                      sd = age_sigma),
                           male = distr(qbern, prob = male_p),
                           n_int = 128,
                           cor = mycor)
