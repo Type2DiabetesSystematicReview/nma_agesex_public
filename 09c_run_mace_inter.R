23533942ac4641036cc4e34e7f53f6c0fcf8276d
David McAllister <david.mcallister@glasgow.ac.uk>
2024-03-14 12:15:47 +0000

    Return to simpler code for mace interaction script as will now drop each trial in turn in a different script

--- 09c_run_mace_inter.R
+++ 09c_run_mace_inter.R
@@ -18,40 +18,36 @@ cfs <- cfs %>%
 
 ## Create networks with different combinations (aggregate = event/hr, main/sg, IPD = pseudo/coefs) ----
 pseudo <- combine_network(set_agd_contrast(data = mace_agg,
-                                          study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                          trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                         set_ipd(data = pseudo, study = nct_id, trt = arm_lvl, r = event,
-                                 trt_ref = "placebo", trt_class = trtcls5))
+                                           study = nct_id, trt = arm_lvl, y = loghr, se = se, 
+                                           trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
+                          set_ipd(data = pseudo, study = nct_id, trt = arm_lvl, r = event,
+                                  trt_ref = "placebo", trt_class = trtcls5))
 # Note - approximately centre but do not scale age by subtracting 60
 pseudo <- pseudo %>%  add_integration(
-                                 age = distr(qtruncnorm, a = min_age-60, b = max_age-60, mean = age_mu-60, sd = age_sigma),
-                                 male = distr(qbern, prob = male_p))
+  age = distr(qtruncnorm, a = min_age-60, b = max_age-60, mean = age_mu-60, sd = age_sigma),
+  male = distr(qbern, prob = male_p))
 mycor <- pseudo$int_cor
 rm(pseudo)
 
-k <- "NCT01131676"
-h <- "NCT00968708"
-j <- "NCT02465515"
-
 if(sg == "main") {
   nwork <- combine_network(set_agd_contrast(data = mace_agg,
-                                           study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                           trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                          set_agd_regression(cfs,
-                                             study = nct_id,
-                                             trt = arm_lvl,
-                                             estimate = estimate,
-                                             se = std.error,
-                                             cor = cors_lst,
-                                             trt_ref = "placebo",
-                                             trt_class = trtcls5,
-                                             regression = myreg))
+                                            study = nct_id, trt = arm_lvl, y = loghr, se = se, 
+                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
+                           set_agd_regression(cfs,
+                                              study = nct_id,
+                                              trt = arm_lvl,
+                                              estimate = estimate,
+                                              se = std.error,
+                                              cor = cors_lst,
+                                              trt_ref = "placebo",
+                                              trt_class = trtcls5,
+                                              regression = myreg))
 }
-if(sg == "sensdrpk") {
+if(sg == "sens") {
   nwork <- combine_network(set_agd_contrast(data = mace_agg,
                                             study = nct_id, trt = arm_lvl, y = loghr, se = se, 
                                             trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == k),
+                           set_agd_regression(cfs %>% filter(!nct_id == "NCT01131676"),
                                               study = nct_id,
                                               trt = arm_lvl,
                                               estimate = estimate,
@@ -62,11 +58,11 @@ if(sg == "sensdrpk") {
                                               regression = myreg))
 }
 
-if(sg == "sensdrph") {
+if(sg == "sens2") {
   nwork <- combine_network(set_agd_contrast(data = mace_agg,
                                             study = nct_id, trt = arm_lvl, y = loghr, se = se, 
                                             trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == h),
+                           set_agd_regression(cfs %>% filter(!nct_id == "NCT00968708"),
                                               study = nct_id,
                                               trt = arm_lvl,
                                               estimate = estimate,
@@ -77,47 +73,15 @@ if(sg == "sensdrph") {
                                               regression = myreg))
 }
 
-if(sg == "sensdrpj") {
-  nwork <- combine_network(set_agd_contrast(data = mace_agg,
-                                            study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == j),
-                                              study = nct_id,
-                                              trt = arm_lvl,
-                                              estimate = estimate,
-                                              se = std.error,
-                                              cor = cors_lst,
-                                              trt_ref = "placebo",
-                                              trt_class = trtcls5,
-                                              regression = myreg))
-}
 
 if(sg == "age") {
   nwork <- combine_network(set_agd_contrast(data = mace_agg_age,
-                                           study = paste0(nct_id, "_", min_age, "_", max_age), trt = arm_lvl, y = loghr, se = se, 
-                                           trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                          set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_age$nct_id),
-                                           study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                           trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                          set_agd_regression(cfs,
-                                             study = nct_id,
-                                             trt = arm_lvl,
-                                             estimate = estimate,
-                                             se = std.error,
-                                             cor = cors_lst,
-                                             trt_ref = "placebo",
-                                             trt_class = trtcls5,
-                                             regression = myreg))
-}
-
-if(sg == "agedrpk") {
-  nwork <- combine_network(set_agd_contrast(data = mace_agg_age,
                                             study = paste0(nct_id, "_", min_age, "_", max_age), trt = arm_lvl, y = loghr, se = se, 
                                             trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
                            set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_age$nct_id),
                                             study = nct_id, trt = arm_lvl, y = loghr, se = se, 
                                             trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == k),
+                           set_agd_regression(cfs,
                                               study = nct_id,
                                               trt = arm_lvl,
                                               estimate = estimate,
@@ -127,69 +91,14 @@ if(sg == "agedrpk") {
                                               trt_class = trtcls5,
                                               regression = myreg))
 }
-
-if(sg == "agedrph") {
-  nwork <- combine_network(set_agd_contrast(data = mace_agg_age,
-                                            study = paste0(nct_id, "_", min_age, "_", max_age), trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_age$nct_id),
-                                            study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == h),
-                                              study = nct_id,
-                                              trt = arm_lvl,
-                                              estimate = estimate,
-                                              se = std.error,
-                                              cor = cors_lst,
-                                              trt_ref = "placebo",
-                                              trt_class = trtcls5,
-                                              regression = myreg))
-}
-
-if(sg == "agedrpj") {
-  nwork <- combine_network(set_agd_contrast(data = mace_agg_age,
-                                            study = paste0(nct_id, "_", min_age, "_", max_age), trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_age$nct_id),
-                                            study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == j),
-                                              study = nct_id,
-                                              trt = arm_lvl,
-                                              estimate = estimate,
-                                              se = std.error,
-                                              cor = cors_lst,
-                                              trt_ref = "placebo",
-                                              trt_class = trtcls5,
-                                              regression = myreg))
-}
-
 if(sg == "sex") {
   nwork <- combine_network(set_agd_contrast(data = mace_agg_sex,
-                                           study = paste0(nct_id, "_", level_cat), trt = arm_lvl, y = loghr, se = se, 
-                                           trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                          set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_sex$nct_id),
-                                           study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                           trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                          set_agd_regression(cfs,
-                                             study = nct_id,
-                                             trt = arm_lvl,
-                                             estimate = estimate,
-                                             se = std.error,
-                                             cor = cors_lst,
-                                             trt_ref = "placebo",
-                                             trt_class = trtcls5,
-                                             regression = myreg))
-}
-
-if(sg == "sexdrpk") {
-  nwork <- combine_network(set_agd_contrast(data = mace_agg_sex,
                                             study = paste0(nct_id, "_", level_cat), trt = arm_lvl, y = loghr, se = se, 
                                             trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
                            set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_sex$nct_id),
                                             study = nct_id, trt = arm_lvl, y = loghr, se = se, 
                                             trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == k),
+                           set_agd_regression(cfs,
                                               study = nct_id,
                                               trt = arm_lvl,
                                               estimate = estimate,
@@ -199,51 +108,12 @@ if(sg == "sexdrpk") {
                                               trt_class = trtcls5,
                                               regression = myreg))
 }
-
-if(sg == "sexdrph") {
-  nwork <- combine_network(set_agd_contrast(data = mace_agg_sex,
-                                            study = paste0(nct_id, "_", level_cat), trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_sex$nct_id),
-                                            study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == h),
-                                              study = nct_id,
-                                              trt = arm_lvl,
-                                              estimate = estimate,
-                                              se = std.error,
-                                              cor = cors_lst,
-                                              trt_ref = "placebo",
-                                              trt_class = trtcls5,
-                                              regression = myreg))
-}
-
-if(sg == "sexdrpj") {
-  nwork <- combine_network(set_agd_contrast(data = mace_agg_sex,
-                                            study = paste0(nct_id, "_", level_cat), trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_contrast(data = mace_agg %>% filter(!nct_id %in% mace_agg_sex$nct_id),
-                                            study = nct_id, trt = arm_lvl, y = loghr, se = se, 
-                                            trt_ref = "placebo", trt_class = trtcls5, sample_size = participants), 
-                           set_agd_regression(cfs %>% filter(!nct_id == j),
-                                              study = nct_id,
-                                              trt = arm_lvl,
-                                              estimate = estimate,
-                                              se = std.error,
-                                              cor = cors_lst,
-                                              trt_ref = "placebo",
-                                              trt_class = trtcls5,
-                                              regression = myreg))
-}
-
-
-
 ## Add integration points. Note taking correlations from pseudo IPD networks
 # Note - approximately centre but do not scale age by subtracting 60
 nwork <-  add_integration(nwork,
-                                age = distr(qtruncnorm,  a = min_age-60, b = max_age-60, mean = age_mu-60, sd = age_sigma),
-                                male = distr(qbern, prob = male_p),
-                               cor = mycor, n_int = 128L)
+                          age = distr(qtruncnorm,  a = min_age-60, b = max_age-60, mean = age_mu-60, sd = age_sigma),
+                          male = distr(qbern, prob = male_p),
+                          cor = mycor)
 mdl <- nma(nwork,
            trt_effects = refe,
            link = "identity",
